5d90d2a5ae16626362ca087ddecd90a1c7a83541
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-04-30 15:42:14 -0300

    bug side bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -101,7 +101,7 @@ export function Navbar() {
           ref={sideBar}
           class="w-1/2 transition-all md:hidden duration-200 -translate-x-[100%] ease-in left-0 bottom-0 h-[calc(100vh_-_80px)] bg-blue-950 fixed z-20"
         >
-          <ul class="flex flex-col w-full gap-5 pt-10">
+          <ul class="flex flex-col justify-center h-full w-full gap-5 pt-10">
             <button onClick={handlerClickSidebar}>
               <LinkItem hrefId="#home" infoLink="Inicio"></LinkItem>
             </button>
