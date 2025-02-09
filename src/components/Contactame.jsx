63f75471c1edf2af5aa38302d3338825460a3412
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2025-02-08 23:36:46 -0300

    Remove Linkedin Icon of Header
    Add devDependencies and button component for Send Email.

--- src/components/Contactame.jsx
+++ src/components/Contactame.jsx
@@ -1,57 +1,53 @@
 import { useEmail } from '../hooks/useEmail.js'
+import { SendMail } from './buttons/SendMail.jsx'
 import './contactame.css'
 
 export function Contactame() {
 
-	const [payload, setPayload, handlerSubmit] = useEmail({
-		to: '',
-		subject: '',
-		message: '',
-	});
+    const [payload, setPayload, handlerSubmit] = useEmail({
+        to: '',
+        subject: '',
+        message: '',
+    });
 
 
-	return (
-		<>
-			<section className="text-2xl p-3 md:p-0 w-full md:w-3/4 mx-10 flex flex-col gap-5">
-				<h2 className="contactame-h2 font-firaCode text-extrabold text-lg sm:text-2xl md:text-3xl lg:text-4xl">Contactame</h2>
-				<form
-					id="contactme"
-					className="flex flex-col gap-5"
-				>
-					<input
-						className="outline-none text-lg dark:text-white text-black p-3 bg-transparent rounded-lg border-2 border-blue-600"
-						type="email"
-						name="to"
-						value={payload.to}
-						onChange={(e) => setPayload({ ...payload, [e.target.name]: e.target.value })}
-						autoComplete="off"
-						placeholder="Correo"
-					/>
-					<input
-						className="outline-none text-lg dark:text-white text-black p-3 bg-transparent rounded-lg border-2 border-blue-600"
-						type="text"
-						name="subject"
-						value={payload.subject}
-						onChange={(e) => setPayload({ ...payload, [e.target.name]: e.target.value })}
-						autoComplete="off"
-						placeholder="Asunto"
-					/>
-					<textarea
-						className="outline-none text-lg dark:text-white text-black p-3 bg-transparent rounded-lg border-2 border-blue-600 h-48 min-h-24 max-h-64"
-						placeholder="Mensaje"
-						autoComplete="off"
-						value={payload.message}
-						onChange={(e) => setPayload({ ...payload, [e.target.name]: e.target.value })}
-						name="message"
-					></textarea>
-					<input
-						type="submit"
-						className="bg-blue-700 dark:hover:bg-blue-500 cursor-pointer rounded-md px-10 py-1 w-max m-auto text-lg"
-						onClick={handlerSubmit}
-						value="Enviar Correo"
-					/>
-				</form>
-			</section>
-		</>
-	)
+    return (
+        <>
+            <section className="text-2xl p-3 md:p-0 w-full md:w-3/4 mx-10 flex flex-col gap-5">
+                <h2 className="contactame-h2 font-firaCode text-extrabold text-lg sm:text-2xl md:text-3xl lg:text-4xl">Contactame</h2>
+                <form
+                    id="contactme"
+                    className="flex flex-col gap-5"
+                >
+                    <input
+                        className="outline-none text-lg dark:text-white text-black p-3 bg-transparent rounded-lg border-2 border-blue-600"
+                        type="email"
+                        name="to"
+                        value={payload.to}
+                        onChange={(e) => setPayload({ ...payload, [e.target.name]: e.target.value })}
+                        autoComplete="off"
+                        placeholder="Correo"
+                    />
+                    <input
+                        className="outline-none text-lg dark:text-white text-black p-3 bg-transparent rounded-lg border-2 border-blue-600"
+                        type="text"
+                        name="subject"
+                        value={payload.subject}
+                        onChange={(e) => setPayload({ ...payload, [e.target.name]: e.target.value })}
+                        autoComplete="off"
+                        placeholder="Asunto"
+                    />
+                    <textarea
+                        className="outline-none text-lg dark:text-white text-black p-3 bg-transparent rounded-lg border-2 border-blue-600 h-48 min-h-24 max-h-64"
+                        placeholder="Mensaje"
+                        autoComplete="off"
+                        value={payload.message}
+                        onChange={(e) => setPayload({ ...payload, [e.target.name]: e.target.value })}
+                        name="message"
+                    ></textarea>
+                    <SendMail handlerSubmit={handlerSubmit} />
+                </form>
+            </section>
+        </>
+    )
 }
--- src/hooks/useEmail.js
+++ src/hooks/useEmail.js
@@ -2,49 +2,59 @@ import { useState } from 'react';
 import { toast } from 'react-toastify'
 
 
+
 export function useEmail({ to, subject, message }) {
-	const [payload, setPayload] = useState({
-		to,
-		subject,
-		message
-	})
+    const [payload, setPayload] = useState({
+        to,
+        subject,
+        message
+    })
 
 
-	const sendEmail = async () => {
-		const requestOptions = {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(payload)
-		};
+    const sendEmail = async () => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(payload)
+        };
+        let toastId;
 
-		try {
-			const toastId = toast.loading("Enviando correo...", {
-				position: "top-left",
-				autoClose: false
-			});
-			const response = await fetch('https://emailapi.ferchudev.com', requestOptions)
-			const data = await response.json();
-			toast.dismiss(toastId);
-			toast.success(data.message, {
-				position: "top-left",
-				autoClose: 3000
-			});
-			setPayload({ to: '', subject: '', message: '' });
-		} catch (error) {
-			toast.error(error.message);
-		}
+        try {
+            toastId = toast.loading("Enviando correo...", {
+                position: "top-left",
+                autoClose: false
+            });
 
-	}
+            const response = await fetch(import.meta.env.PUBLIC_API_URL_EMAIL, requestOptions)
+            console.log('.env:', import.meta.env);
+            const data = await response.json();
+            toast.dismiss(toastId);
+            toast.success(data.message, {
+                position: "top-left",
+                autoClose: 1000
+            });
+            setPayload({ to: '', subject: '', message: '' });
+        } catch (error) {
+            toast.dismiss(toastId);
+            toast.error("Error al enviar el correo", {
+                position: "top-left",
+                autoClose: 1000
+            });
+
+            console.log('.env:', import.meta.env);
+        }
+
+    }
 
 
-	const handlerSubmit = (e) => {
-		e.preventDefault();
-		sendEmail();
-	}
+    const handlerSubmit = (e) => {
+        e.preventDefault();
+        sendEmail();
+    }
 
-	return [
-		payload,
-		setPayload,
-		handlerSubmit
-	];
+    return [
+        payload,
+        setPayload,
+        handlerSubmit
+    ];
 }
